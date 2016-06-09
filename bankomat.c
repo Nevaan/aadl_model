bb9e6d481fd30a81565d1ec05f645f1d1d455787
Nevaan <pawel.losek@best.krakow.pl>
2016-06-09 23:55:10 +0200

    Added subprograms

--- bankomat.c
+++ bankomat.c
@@ -1,1 +1,48 @@
 #include <stdio.h>
+
+void get_content();
+void send_text();
+void read_key_input();
+void wydaj_kwote();
+void pobierz_stan_konta();
+void wplata_gotowki();
+void wyplata_gotowki();
+void czytaj_karte();
+void wydaj_karte();
+
+void get_content() {
+	// wyslij dane do wyswietlenia
+};
+
+void send_text(){
+	// wyslij text do wydrukowania
+};
+
+void read_key_input(){
+
+}
+
+void wydaj_kwote() {
+
+}
+
+void pobierz_stan_konta() {
+
+};
+
+
+void wplata_gotowki(){
+
+};
+
+void wyplata_gotowki(){
+
+};
+void czytaj_karte(){ 
+
+};
+
+
+void wydaj_karte(){
+
+};
