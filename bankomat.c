4b0d366ae4115908a8c1060f8457c9b759399ca1
Nevaan <pawel.losek@best.krakow.pl>
2016-06-10 03:17:54 +0200

    added c file

--- bankomat.c
+++ bankomat.c
@@ -1,48 +1,72 @@
 #include <stdio.h>
 
-void get_content();
-void send_text();
-void read_key_input();
-void wydaj_kwote();
-void pobierz_stan_konta();
-void wplata_gotowki();
-void wyplata_gotowki();
+/* DaneBankowe dane_bankowe; */
+char wybrana_opcja;
+int stan_sejfu;
+
+String get_content();
+String send_text();
+void read_key_input(char keyboard_in);
+bool wydaj_kwote(int kwota);
+int pobierz_stan_konta();
+void wplata_gotowki(int kwota);
+void wyplata_gotowki(int kwota);
 void czytaj_karte();
 void wydaj_karte();
+void get_dane_konta();
+void send_dane_karty();
 
-void get_content() {
-	// wyslij dane do wyswietlenia
+String get_content() {
+	// wyslij dane do wyswietlenia - odpowiedni ekran
+	return content;
 };
 
-void send_text(){
-	// wyslij text do wydrukowania
+String send_text(){
+	// wyslij text do wydrukowania - raport transakcji
+	return data;
 };
 
-void read_key_input(){
-
+void read_key_input(char keyboard_in){
+	// podejmij odpowiednia akcje w zaleznosci od inputu
 }
 
-void wydaj_kwote() {
-
+bool wydaj_kwote(int kwota) {
+	if (stan_sejfu >= kwota){
+		// sygna³ do sejfu - przekaz okreslona kwote do modulu gotowkowego
+		return true;
+	} else {
+		return false;
+	}
 }
 
-void pobierz_stan_konta() {
-
+int pobierz_stan_konta() {
+	return stan_sejfu;
 };
 
 
-void wplata_gotowki(){
-
+void wplata_gotowki(int kwota){
+	//sygnal do modulu gotowkowego - otworz i pobierz banknoty
 };
 
-void wyplata_gotowki(){
-
+void wyplata_gotowki(int kwota){
+	//sygnal do modulu gotowkowego - otworz i wydaj banknoty
 };
+
 void czytaj_karte(){ 
-
+	//odczytaj dane karty
+	send_dane_karty();
 };
 
 
 void wydaj_karte(){
+	// sygnal do czytnika - wydaj karte
+};
 
+/*DaneBankowe */ get_dane_konta(){
+	/* return dane_bankowe; */
+};
+
+void send_dane_karty(){
+	// wyslij request o dane bankowe - autoryzacja
+	// dane_bankowe = response;
 };
